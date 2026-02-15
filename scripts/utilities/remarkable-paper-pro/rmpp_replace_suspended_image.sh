c91c43d93812cee0e8f08eada4276fde85460e4d
Matthew Janssen <a24f2322_80b6_4016_b1a6_531132a59822@pobox.com>
2026-02-14 19:06:16 -0600

    Update rmpp_replace_suspended_image.sh

--- scripts/utilities/remarkable-paper-pro/rmpp_replace_suspended_image.sh
+++ scripts/utilities/remarkable-paper-pro/rmpp_replace_suspended_image.sh
@@ -4,11 +4,23 @@ ssh -t rmpp << 'EOF'
 echo 'Remounting...'
 mount -o remount,rw /
 
-echo 'Renaming...'
-mv /usr/share/remarkable/suspended.png /usr/share/remarkable/suspended.png.bak
+echo 'Removing...'
+# On older firmware, this was the only image used for the sleeping screen.
+# mv /usr/share/remarkable/suspended.png /usr/share/remarkable/suspended.png.bak
+
+# On newer firmware, there are three images used for the sleeping screen, and they are in a different location.
+rm /usr/share/remarkable/carousel/sleep_Illustration_01.png
+rm /usr/share/remarkable/carousel/sleep_Illustration_02.png
+rm /usr/share/remarkable/carousel/sleep_Illustration_03.png
 
 echo 'Replacing...'
-cp /home/root/suspended.png /usr/share/remarkable/suspended.png
+# On older firmware, this was the only image used for the sleeping screen.
+# cp /home/root/suspended.png /usr/share/remarkable/suspended.png
+
+# On newer firmware, there are three images used for the sleeping screen, and they are in a different location.
+cp /home/root/suspended.png /usr/share/remarkable/carousel/sleep_Illustration_01.png
+cp /home/root/suspended.png /usr/share/remarkable/carousel/sleep_Illustration_02.png
+cp /home/root/suspended.png /usr/share/remarkable/carousel/sleep_Illustration_03.png
 
 echo 'Rebooting...'
 reboot
